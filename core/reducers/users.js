3beea2e5f9afc0a14b5b48e26cf4cb9113cf2ea3
Jorge Salcedo <jorgeto86@gmail.com>
2018-09-25 21:10:16 +0200

    Initial commit

---
+++ core/reducers/users.js
@@ -0,0 +1,11 @@
+// import { ADD_TODO, TOGGLE_TODO } from "../actionTypes";
+
+const initialState = {
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
